c74e855f1245d5a3366a72baaf74d8bd7c3798dd
erlangga0 <erlangga.maulana@gmail.com>
2025-06-08 06:53:16 +0700

    Add main App component with providers

---
+++ client/src/pages/_app.tsx
@@ -0,0 +1,40 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+import { Toaster } from 'react-hot-toast'
+import { AuthProvider } from '../contexts/AuthContext'
+import Layout from '../components/Layout'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 1,
+      staleTime: 5 * 60 * 1000, // 5 minutes
+    },
+  },
+})
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+        <Toaster
+          position="top-right"
+          toastOptions={{
+            duration: 4000,
+            style: {
+              background: '#1f2937',
+              color: '#f9fafb',
+            },
+          }}
+        />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </AuthProvider>
+    </QueryClientProvider>
+  )
+}
